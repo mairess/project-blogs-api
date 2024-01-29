17fd5061c41b29bdc305630e6198c09fdc620b39
mairess <trabalho.maires@gmail.com>
2024-01-29 19:26:27 -0300

    refactor: sort http status

--- src/utils/mapStatusHTTP.js
+++ src/utils/mapStatusHTTP.js
@@ -1,11 +1,11 @@
 const httpErrorMap = {
   SUCCESSFUL: 200,
   CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
   NOT_FOUND: 404,
   CONFLICT: 409,
   INVALID_VALUE: 422,
-  BAD_REQUEST: 400,
-  UNAUTHORIZED: 401,
 };
   
 const mapStatusHTTP = (status) => httpErrorMap[status] || 500;
