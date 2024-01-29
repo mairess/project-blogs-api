551a057e1dabfad55475880f8fd63dff84c86ea6
mairess <trabalho.maires@gmail.com>
2024-01-29 15:24:39 -0300

    feat: POST /login

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { userController } = require('./controllers');
 
 // ...
 
@@ -11,6 +12,7 @@ app.get('/', (_request, response) => {
 
 app.use(express.json());
 
+app.post('/login', userController.login);
 // ...
 
 // É importante exportar a constante `app`,
