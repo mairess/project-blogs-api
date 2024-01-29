33b4b1428fb66706c8b2f7cdb4bb0d26348b2b1f
mairess <trabalho.maires@gmail.com>
2024-01-29 17:42:48 -0300

    refactor: createToken

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,17 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const secret = process.env.JWT_SECRET || 'xxDairhd666654sdXdfTOKFCC';
 
-const createToken = (user) => {
+const createToken = (payload) => {
   const jwtConfig = {
     expiresIn: '1d',
     algorithm: 'HS256',
   };
 
-  const token = jwt.sign(
-    { username: user.email },
-    secret,
-    jwtConfig,
-  );
+  const token = jwt.sign(payload, secret, jwtConfig);
 
   return token;
 };
