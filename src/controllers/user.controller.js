4746816273c9c93e6e5c258b3d03a5455c183e20
mairess <trabalho.maires@gmail.com>
2024-01-29 15:22:53 -0300

    feat: user controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,13 @@
+const userService = require('../services/user.service');
+const mapStatusHTTP = require('../utils/mapStatusHTTP');
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  
+  const { status, data } = await userService.login({ email, password });
+  res.status(mapStatusHTTP(status)).json(data);
+};
+
+module.exports = {
+  login,
+};
