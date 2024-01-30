82de027b9743daf41a31c03695d6d4fe952a4d32
mairess <trabalho.maires@gmail.com>
2024-01-30 19:55:06 -0300

    feat: autoincrement id

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
       id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
+        autoIncrement: true,
       },
       title: DataTypes.STRING,
       content: DataTypes.STRING,
