abf8cd068b3b50e107d1abeca663c588159d52c8
mairess <trabalho.maires@gmail.com>
2024-01-30 07:02:27 -0300

    feat: GET route /user/id

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,5 +4,6 @@ const authMiddleware = require('../middlewares/auth');
 
 route.post('/user', userController.creteNewUser);
 route.get('/user', authMiddleware, userController.getAll);
+route.get('/user/:id', authMiddleware, userController.getById);
 
 module.exports = route;
