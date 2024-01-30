e9bfb80a826c0747af3b26fc38543845c32d357e
mairess <trabalho.maires@gmail.com>
2024-01-30 06:42:05 -0300

    feat: GET route /user

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,8 @@
 const route = require('express').Router();
 const { userController } = require('../controllers');
+const authMiddleware = require('../middlewares/auth');
 
 route.post('/user', userController.creteNewUser);
+route.get('/user', authMiddleware, userController.getAll);
 
 module.exports = route;
