6477746af15e0d8fac37b826efa451393238eb8c
mairess <trabalho.maires@gmail.com>
2024-01-31 09:56:17 -0300

    feat: sequelize transaction

--- src/controllers/blogPost.controller.js
+++ src/controllers/blogPost.controller.js
@@ -5,13 +5,17 @@ const createNewPost = async (req, res) => {
   const { title, content, categoryIds } = req.body;
   const { email } = req.user.user;
   
-  const { status, data } = await blogPostService.createNewPost({ 
-    title, 
-    content,
-    categoryIds,
-    email,
-  });
-  res.status(mapStatusHTTP(status)).json(data);
+  try {
+    const { status, data } = await blogPostService.createNewPost({ 
+      title, 
+      content,
+      categoryIds,
+      email,
+    });
+    res.status(mapStatusHTTP(status)).json(data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 module.exports = {
--- src/services/blogPost.service.js
+++ src/services/blogPost.service.js
@@ -1,25 +1,42 @@
+const Sequelize = require('sequelize');
 const { BlogPost, User, Category, PostCategory } = require('../models');
 const schema = require('./validations/validationInputValues');
+const config = require('../config/config');
 
-const createNewPost = async (blogPost) => {
-  const error = schema.validateBlogPost(blogPost);
-  const { title, content, categoryIds, email } = blogPost;
+const env = process.env.NODE_ENV || 'development';
+const sequelize = new Sequelize(config[env]);
+
+const validateCategories = async (categoryIds) => {
+  const arrayOfPromises = categoryIds.map(async (id) => {
+    const category = await Category.findByPk(id);
+    if (!category) {
+      throw new Error('one or more "categoryIds" not found');
+    }
+  });
+  await Promise.all(arrayOfPromises);
+};
+
+const associateCategoryWithPost = async (postId, categoryIds, transaction) => {
+  const arrayOfPostCategory = categoryIds.map((categoryId) => ({ postId, categoryId }));
+  await PostCategory.bulkCreate(arrayOfPostCategory, transaction);
+};
+
+const createNewPost = async ({ title, content, categoryIds, email }) => {
+  const error = schema.validateBlogPost({ title, content, categoryIds, email });
   if (error) return { status: 'BAD_REQUEST', data: { message: error.message } };
-  try {
-    const arrayOfPromises = categoryIds.map(async (id) => {
-      const category = await Category.findByPk(id);
-      if (!category) throw new Error('one or more "categoryIds" not found');
-    });
-    await Promise.all(arrayOfPromises);
-  } catch (err) { return { status: 'BAD_REQUEST', data: { message: err.message } }; }
-  const theUser = await User.findOne({ where: { email: blogPost.email } });
-  const thePost = await BlogPost.create({ title, content, categoryIds, email, userId: theUser.id });
-  await PostCategory.bulkCreate([
-    { postId: thePost.id, categoryId: 1 },
-    { postId: thePost.id, categoryId: 2 },
-  ]);
-  
-  return { status: 'CREATED', data: thePost };
+  const result = await sequelize.transaction(async (t) => {
+    try { 
+      await validateCategories(categoryIds);
+    } catch (err) { 
+      throw new Error(err.message);
+    }
+    const user = await User.findOne({ where: { email } });
+    const post = await BlogPost
+      .create({ title, content, categoryIds, email, userId: user.id }, { transaction: t });
+    await associateCategoryWithPost(post.id, categoryIds, { transaction: t });
+    return post;
+  });
+  return { status: 'CREATED', data: result };
 };
 
 module.exports = {
