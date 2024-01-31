f20155ae5a65ad54a743136cf9d16f802a729ffe
mairess <trabalho.maires@gmail.com>
2024-01-31 17:29:02 -0300

    feat: PUT route /post/id

--- src/routes/blogPost.route.js
+++ src/routes/blogPost.route.js
@@ -5,5 +5,6 @@ const authMiddleware = require('../middlewares/auth');
 route.post('/post', authMiddleware, blogPostController.createNewPost);
 route.get('/post', authMiddleware, blogPostController.getAll);
 route.get('/post/:id', authMiddleware, blogPostController.getById);
+route.put('/post/:id', authMiddleware, blogPostController.update);
 
 module.exports = route;
