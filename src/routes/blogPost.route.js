f882db4ce842fd98d942bff8be66ab72b25d3909
mairess <trabalho.maires@gmail.com>
2024-01-31 11:39:22 -0300

    feat: GET route /post

--- src/routes/blogPost.route.js
+++ src/routes/blogPost.route.js
@@ -3,5 +3,6 @@ const { blogPostController } = require('../controllers');
 const authMiddleware = require('../middlewares/auth');
 
 route.post('/post', authMiddleware, blogPostController.createNewPost);
+route.get('/post', authMiddleware, blogPostController.getAll);
 
 module.exports = route;
