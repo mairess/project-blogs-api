49748055fb2e93440538000a92d70a6daef48026
mairess <trabalho.maires@gmail.com>
2024-01-31 17:27:39 -0300

    feat: extract service createNewPost to a single funtion

---
+++ src/services/createNewPost.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const config = require('../config/config');
+const { validateBlogPost,
+  validateCategories, 
+  associateCategoryWithPost,
+} = require('./validations');
+const { BlogPost, User } = require('../models');
+
+const env = process.env.NODE_ENV || 'development';
+const sequelize = new Sequelize(config[env]);
+
+const createNewPost = async (postData) => {
+  const error = validateBlogPost(postData);
+  if (error) return { status: 'BAD_REQUEST', data: { message: error.message } };
+  const result = await sequelize.transaction(async (t) => {
+    try { 
+      await validateCategories(postData.categoryIds);
+    } catch (err) { 
+      throw new Error(err.message);
+    }
+    const user = await User.findOne({ where: { email: postData.email } });
+    const post = await BlogPost.create({ ...postData, userId: user.id }, { transaction: t });
+    await associateCategoryWithPost(post.id, postData.categoryIds, { transaction: t });
+    return post;
+  });
+  return { status: 'CREATED', data: result };
+};
+
+module.exports = createNewPost;
