d3496dc585216f9ea1fd1b4323178acaf653d8b9
mairess <trabalho.maires@gmail.com>
2024-02-01 07:32:35 -0300

    feat: DELETE route /post/id

--- src/routes/blogPost.route.js
+++ src/routes/blogPost.route.js
@@ -6,5 +6,6 @@ route.post('/post', authMiddleware, blogPostController.createNewPost);
 route.get('/post', authMiddleware, blogPostController.getAll);
 route.get('/post/:id', authMiddleware, blogPostController.getById);
 route.put('/post/:id', authMiddleware, blogPostController.update);
+route.delete('/post/:id', authMiddleware, blogPostController.deletePost);
 
 module.exports = route;
