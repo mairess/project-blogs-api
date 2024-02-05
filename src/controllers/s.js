f6b52baf43ccda6a3471a633e576051d9d5cde94
mairess <trabalho.maires@gmail.com>
2024-02-05 07:32:48 -0300

    feat: remove unnecessary files

--- src/controllers/s.js
+++
@@ -1,9 +0,0 @@
-const userController = require('./user.controller');
-const categoryController = require('./category.controller');
-const blogPostController = require('./blogPost.controller');
-
-module.exports = {
-  userController,
-  categoryController,
-  blogPostController,
-};
--- src/services/as.js
+++
@@ -1,9 +0,0 @@
-const userService = require('./user.service');
-const categoryService = require('./category.service');
-const blogPostService = require('./blogPost.service');
-
-module.exports = {
-  userService,
-  categoryService,
-  blogPostService,
-};
