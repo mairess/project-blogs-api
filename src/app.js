4da5f1e1db5c85cc95753923cc58c0d12a17ddbc
mairess <trabalho.maires@gmail.com>
2024-04-06 10:07:09 -0300

    feat: partial documentation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const cors = require('cors');
 const { loginRoutes, userRoutes, categoryRoutes, blogPostRoutes } = require('./routes');
 require('express-async-errors');
+
+const raw = fs.readFileSync('swagger.yaml');
+const data = yaml.load(raw);
 
 const erroMessage500 = 'Unexpected error!';
 
@@ -12,10 +19,13 @@ app.get('/', (_request, response) => {
 });
 
 app.use(express.json());
+app.use(cors());
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(data));
 app.use('/login', loginRoutes);
 app.use('/user', userRoutes);
 app.use('/categories', categoryRoutes);
+
 app.use('/post', blogPostRoutes);
 
 app.use((error, req, res, _next) => {
